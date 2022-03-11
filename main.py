16bd9e4a8734096cf2fe6512169a87bdb5d09047
Laddge <dev.laddge@gmail.com>
2022-03-11 11:46:25 +0900

    [feat] 解法をもとにキューブを動かす関数を実装

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import kociemba
 import RPi.GPIO as GPIO
 import time
+from threading import Thread
 
 # 持ち手の向き 垂直=>0 水平=>1
 HOR = [0, 0, 0, 0]  # [L, R, F, B]
@@ -63,3 +64,144 @@ def chwid(drc, grab):
     time.sleep(.3)
 
     GPIO.cleanup(pin)
+
+
+def run(movestr):
+    moves = movestr.strip().split(" ")
+    while moves:
+        if moves[0][0] == "L" or moves[0][0] == "R":
+            if HOR[2] and HOR[3]:
+                chwid(1, 0)
+                t0 = Thread(target=rotate, args=(2, 0))
+                t1 = Thread(target=rotate, args=(3, 0))
+                t0.start()
+                t1.start()
+                t0.join()
+                t1.join()
+            elif HOR[2]:
+                chwid(1, 0)
+                rotate(2, 0)
+            elif HOR[3]:
+                chwid(1, 0)
+                rotate(3, 0)
+            chwid(1, 1)
+            if moves[0][0] == "L":
+                mtr0 = 0
+            else:
+                mtr0 = 1
+            if len(moves[0]) == 1:
+                mode0 = 0
+            elif moves[0][1] == "'":
+                mode0 = 1
+            else:
+                mode0 = 2
+            if moves[1][0] == "L" or moves[1][0] == "R":
+                if moves[1][0] == "L":
+                    mtr1 = 0
+                else:
+                    mtr1 = 1
+                if len(moves[1]) == 1:
+                    mode1 = 0
+                elif moves[1][1] == "'":
+                    mode1 = 1
+                else:
+                    mode1 = 2
+                t0 = Thread(target=rotate, args=(mtr0, mode0))
+                t1 = Thread(target=rotate, args=(mtr1, mode1))
+                t0.start()
+                t1.start()
+                t0.join()
+                t1.join()
+                moves = moves[2:]
+            else:
+                rotate(mtr0, mode0)
+                moves = moves[1:]
+        elif moves[0][0] == "F" or moves[0][0] == "B":
+            if HOR[0] and HOR[1]:
+                chwid(0, 0)
+                t0 = Thread(target=rotate, args=(0, 0))
+                t1 = Thread(target=rotate, args=(1, 0))
+                t0.start()
+                t1.start()
+                t0.join()
+                t1.join()
+            elif HOR[0]:
+                chwid(0, 0)
+                rotate(0, 0)
+            elif HOR[1]:
+                chwid(0, 0)
+                rotate(1, 0)
+            chwid(0, 1)
+            if moves[0][0] == "F":
+                mtr0 = 2
+            else:
+                mtr0 = 3
+            if len(moves[0]) == 1:
+                mode0 = 0
+            elif moves[0][1] == "'":
+                mode0 = 1
+            else:
+                mode0 = 2
+            if moves[1][0] == "F" or moves[1][0] == "B":
+                if moves[1][0] == "F":
+                    mtr1 = 2
+                else:
+                    mtr1 = 3
+                if len(moves[1]) == 1:
+                    mode1 = 0
+                elif moves[1][1] == "'":
+                    mode1 = 1
+                else:
+                    mode1 = 2
+                t0 = Thread(target=rotate, args=(mtr0, mode0))
+                t1 = Thread(target=rotate, args=(mtr1, mode1))
+                t0.start()
+                t1.start()
+                t0.join()
+                t1.join()
+                moves = moves[2:]
+            else:
+                rotate(mtr0, mode0)
+                moves = moves[1:]
+        else:
+            if HOR[0] == HOR[1] == 0:
+                if HOR[2] and HOR[3]:
+                    chwid(1, 0)
+                    t0 = Thread(target=rotate, args=(2, 0))
+                    t1 = Thread(target=rotate, args=(3, 0))
+                    t0.start()
+                    t1.start()
+                    t0.join()
+                    t1.join()
+                elif HOR[2]:
+                    chwid(1, 0)
+                    rotate(2, 0)
+                elif HOR[3]:
+                    chwid(2, 0)
+                    rotate(3, 0)
+                chwid(1, 1)
+            if HOR[0] == HOR[1]:
+                chwid(0, 0)
+                rotate(0, 0)
+                chwid(0, 1)
+            chwid(1, 0)
+            t0 = Thread(target=rotate, args=(0, 0))
+            t1 = Thread(target=rotate, args=(1, 1))
+            t0.start()
+            t1.start()
+            t0.join()
+            t1.join()
+            chwid(1, 1)
+            new = []
+            for move in moves:
+                if move[0] == "U":
+                    new.append(move.replace("U", "F"))
+                elif move[0] == "F":
+                    new.append(move.replace("F", "D"))
+                elif move[0] == "D":
+                    new.append(move.replace("D", "B"))
+                elif move[0] == "B":
+                    new.append(move.replace("B", "U"))
+                else:
+                    new.append(move)
+            moves = new
