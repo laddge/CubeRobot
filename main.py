140b68326a23479b8b03d324ec7122320ee08334
Laddge <dev.laddge@gmail.com>
2022-03-11 11:56:58 +0900

    [feat] サーボモーターの角度を変数で管理しやすくした

--- main.py
+++ main.py
@@ -56,10 +56,8 @@ def chwid(drc, grab):
     pwm = GPIO.PWM(pin, 50)
     pwm.start(0.0)
 
-    if grab:
-        dc = 4.08
-    else:
-        dc = 10.42
+    dcs = [[10.42, 4.08], [10.42, 4.08]]
+    dc = dcs[drc][grab]
     pwm.ChangeDutyCycle(dc)
     time.sleep(.3)
 
